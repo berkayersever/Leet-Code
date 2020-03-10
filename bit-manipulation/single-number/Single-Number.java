379105666ec7dc201fa9cabce992d2e6c9d6af15
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-10 17:07:38 +0300

    Create Single-Number.java
    https://leetcode.com/problems/single-number/

---
+++ bit-manipulation/single-number/Single-Number.java
@@ -0,0 +1,9 @@
+class Single-Number {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
