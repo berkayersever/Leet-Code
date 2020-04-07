5f91ce06b7e1a7db0323365cb9d5f68bc1063895
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-07 03:01:46 +0300

    Create Move-Zeroes.java
    https://leetcode.com/problems/move-zeroes/

---
+++ array/move-zeroes/Move-Zeroes.java
@@ -0,0 +1,15 @@
+class Move-Zeroes {
+    public void moveZeroes(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                count++;
+            }
+            else if(count > 0) {
+                int t = nums[i];
+                nums[i] = 0;
+                nums[i - count] = t;
+            }
+        }
+    }
+}
