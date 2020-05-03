a85b630c70e57ab1c264b81b58de8af8bd7ed802
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-05-03 23:36:04 +0300

    Create Construct-Binary-Search-Tree-from-Preorder-Traversal.java
    https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/

---
+++ tree/construct-binary-search-tree-from-preorder-traversal/Construct-Binary-Search-Tree-from-Preorder-Traversal.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Construct-Binary-Search-Tree-from-Preorder-Traversal {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        
+        int n = preorder.length;
+        if (n == 0) return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> deque = new ArrayDeque<TreeNode>();
+        deque.push(root);
+        
+        for (int i = 1; i < n; i++) {
+            TreeNode node = deque.peek();
+            TreeNode child = new TreeNode(preorder[i]);
+            while (!deque.isEmpty() && deque.peek().val < child.val)
+                node = deque.pop();
+            
+            if (node.val < child.val) node.right = child;
+            else node.left = child;
+            deque.push(child);
+        }
+        return root;
+    }
+}
