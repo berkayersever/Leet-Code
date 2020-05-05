4194338fed6a499874ff47ce3c8fc8d3030c8462
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-05-05 23:42:32 +0300

    Update Binary-Tree-Maximum-Path-Sum.java
    https://leetcode.com/problems/binary-tree-maximum-path-sum/

--- tree/binary-tree-maximum-path-sum/Binary-Tree-Maximum-Path-Sum.java
+++ tree/binary-tree-maximum-path-sum/Binary-Tree-Maximum-Path-Sum.java
@@ -18,10 +18,18 @@ class Binary-Tree-Maximum-Path-Sum {
 
   public int max_gain(TreeNode node) {
     if (node == null) return 0;
+	
+    // Max sum on the left and right sub-trees of node
     int left_gain = Math.max(max_gain(node.left), 0);
     int right_gain = Math.max(max_gain(node.right), 0);
+
+    // The price to start a new path where 'node' is a highest node
     int price_newpath = node.val + left_gain + right_gain;
+
+    // Update max_sum if it's better to start a new path
     max_sum = Math.max(max_sum, price_newpath);
+
+    // For Recursion => Return the max gain if continue the same path
     return node.val + Math.max(left_gain, right_gain);
   }
 
